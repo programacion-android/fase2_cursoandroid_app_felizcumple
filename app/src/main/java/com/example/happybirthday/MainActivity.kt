f0924b2955612e4740d611789963cdfb222c3058
encarna <ebalseraq01@educarex.es>
2026-09-03 19:19:48 +0200

    Commit2: Agregar texto

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,38 +17,56 @@ import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //TODO: Agrega un nuevo elemento  https://developer.android.com/codelabs/basic-android-kotlin-compose-text-composables?hl=es-419&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-1-pathway-3%3Fhl%3Des-419%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-text-composables#4
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                /* Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                         name = "Android",
+                         modifier = Modifier.padding(innerPadding)
+                     )
+                 }*/
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
                 }
             }
         }
     }
 }
 
-@Composable
+/*@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
-}
+}*/
 
-@Preview(showBackground = true,
-   // backgroundColor = 0xff334455,
-    name="Mi preview"/*,
+@Preview(
+    showBackground = true,
+    // backgroundColor = 0xff334455,
+    name = "Mi preview"/*,
     showSystemUi = true*/
 )
 //@Preview
 @Composable
 fun GreetingPreview() {
     HappyBirthdayTheme {
-        Greeting("Encarna")
+        // Greeting("Encarna")
+
+        GreetingText(message = "Happy Birthday Encarna!")
     }
+}
+
+@Composable
+fun GreetingText(message: String, modifier: Modifier = Modifier) {
+    Text(
+        text = message
+    )
 }
