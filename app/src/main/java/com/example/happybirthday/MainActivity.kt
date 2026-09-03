61eb574369e373d56d7e75e0cada444c72298fe2
encarna <ebalseraq01@educarex.es>
2026-09-03 20:19:59 +0200

    Commit3:cambiar tamaño letra

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.happybirthday
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -67,6 +69,10 @@ fun GreetingPreview() {
 @Composable
 fun GreetingText(message: String, modifier: Modifier = Modifier) {
     Text(
-        text = message
+        text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.5.sp
+
+
     )
 }
